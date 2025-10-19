d8bb502fd00200c96048aac45258588fe1384234
grilled-swampert <128573732+grilled-swampert@users.noreply.github.com>
2025-10-19 17:46:44 +0530

    initial setup

---
+++ __init__.py
@@ -0,0 +1,9 @@
+bl_info = {
+    "name": "Polyline Manager",
+    "author": "crisplettuce",
+    "version": (2, 0, 0),
+    "blender": (3, 0, 0),
+    "location": "View3D > Sidebar > Polyline",
+    "description": "Advanced polyline management with collision detection, rotation, and multiple curve types",
+    "category": "3D View",
+}
